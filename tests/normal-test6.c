50676bf35210fa9662fda3212e7fa191d4f64ed9
Yile Gu <yilegu@umich.edu>
2022-12-12 13:29:30 -0500

    Add move intervening code down example

---
+++ tests/normal-test6.c
@@ -0,0 +1,27 @@
+// example of moving down intervening code
+#include <stdio.h>
+
+int main() {
+    int x = 1;
+    int y = 1;
+    int z;
+    int A[2] = {10,11};
+
+    scanf("%d", &z);
+    // these two loops should be merged
+    for (int i = 0; i < 5; ++i) {
+        x = x + i;
+    }
+
+
+    if (z) {
+        z = x * 5;
+    }
+    for (int j = 0; j < 5; ++j) {
+        y = y * (1 + j);
+    }
+
+    // printing
+    printf("x: %d y: %d\n", x, y);
+    printf("zzzzzz: %d \n", z);
+}
